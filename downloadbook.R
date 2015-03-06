ff96f89af4ba9176101c41516c8974a13be435f7
bw4sz <benweinstein2010@gmail.com>
2015-03-06 10:28:20 -0800

    webscrape

---
+++ downloadbook.R
@@ -0,0 +1,32 @@
+library(XML)
+library(stringr)
+library(rvest)
+library(dplyr)
+
+url<-"http://git.psylab.info/r-library/src/25635d3e6d3d/bibs/en-books.bib?at=master"
+p<- html(url)
+
+p %>% html_node("url")
+tables <- readHTMLTable(url,header = TRUE,trim=T)
+
+tables[[1]]
+names(tables)<-"t"
+str(tables)
+string<-as.character(tables[[1]][1,"V2"])
+
+#get everythhing after url
+
+#oops looks like we have a trailing brace
+r<-sapply(r,function(x){
+gsub(x,pattern="}",replacement="")
+})
+
+#download and name
+
+for(book in 1:length(r)){
+#get filename
+filnam<-str_match(book,"(\\w+).pdf")[,1]
+
+#download each
+download.file(book,paste("C:\\Users\\Ben\\Dropbox\\Rbooks",filnam,sep="\\"),mode="wb")
+}
